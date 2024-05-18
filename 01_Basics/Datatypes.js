8e03c529df4e13f110470445cc9654e56ceef521
Arvind Krishna Yadav <156197490+arvindkri-03@users.noreply.github.com>
2024-05-18 08:04:15 +0000

    Learn Datatypes

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"    // It treats all js code as latest version.
+//alert(3+3)       We are using nodejs not any browser
+
+console.log(3+3);  
+
+console.log(3
+    +
+    3);  // code readabilitty should be high.
+
+    console.log('shambhaa')
+
+    let name="Arvind"
+    let age=20
+    let isWorkdone=true
+    let state
+
+    console.log(name,age,isWorkdone,state)
+
+    // number => 2 to power 53
+    // bigint  =>  (Undefined)   represent numeric values which are too large 
+    // string => ""
+    // boolean => true/false
+    // null => standalone value object
+    // undefined => undefined
+    // symbol => unique
+    // object
+
+    console.log(typeof undefined);
+    console.log(typeof null);
+    console.log(typeof bigint);
