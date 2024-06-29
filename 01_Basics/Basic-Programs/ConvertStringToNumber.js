c8210b8504b7feace3d819c685a390bf7af22225
Arvind Krishna Yadav <156197490+arvindkri-03@users.noreply.github.com>
2024-06-29 10:40:03 +0000

    conversion of string to number

---
+++ 01_Basics/Basic-Programs/ConvertStringToNumber.js
@@ -0,0 +1,10 @@
+let str = "100.2"
+
+let num = +str
+
+//console.log(num);
+// console.log(Number(str)); 
+// console.log(+str);
+console.log(parseInt(str)); // NaN
+console.log(parseFloat(str)); // NaN
+
